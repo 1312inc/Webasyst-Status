c6bf85c03dca0fbc18bc59625e9d2a10d9dd7fef
Kirill Maramygin <kirill.maramygin@gmail.com>
2019-11-30 13:40:07 +0300

    7.34:add 'No status' to the .s-editor status selector

--- lib/actions/today/statusTodaystatusSave.controller.php
+++ lib/actions/today/statusTodaystatusSave.controller.php
@@ -19,73 +19,78 @@ class statusTodaystatusSaveController extends statusJsonController
         }
         $status = statusTodayStatusFactory::getForContactId(stts()->getUser()->getContactId(), $date);
 
-        $calendar = stts()->getModel('waContactCalendars')->getById($data['calendar_id']);
-        if (empty($calendar)) {
-            $this->setError('No calendar');
-
-            return;
-        }
-
-        $data['summary'] = !empty($data['summary']) ? $data['summary'] : $calendar['default_status'];
-
         /** @var waContactEventsModel $waContactEventsModel */
         $waContactEventsModel = stts()->getModel('waContactEvents');
-        if (!$status->getCalendarId()
-            || ($status->getCalendarId() && $status->getDays() > 1)
-            || !empty($data['brand_new'])
-        ) {
-            $data['status_id'] = $waContactEventsModel->insertEvent(
-                [
-                    'calendar_id' => $data['calendar_id'],
-                    'summary' => $data['summary'],
-                    'start' => $date->format('Y-m-d H:00:00'),
-                    'end' => min(
-                        $date->format('Y-m-d 23:59:59'),
-                        (clone $date)->modify('+1 hour')->format('Y-m-d H:00:00')
-                    ),
-                    'is_allday' => 1,
-                    'is_status' => 1,
-                    'summary_type' => !empty($data['summary']) ? 'custom' : 'default',
-                    'update_datetime' => date('Y-m-d H:i:s'),
-                    'description' => '',
-                    'location' => '',
-                ]
-            );
-            $this->logAction('event_add', $data['status_id'], wa()->getUser()->getId());
 
-            (new waLogModel())->insert(
-                [
-                    'app_id' => 'team',
-                    'contact_id' => wa()->getUser()->getId(),
-                    'params' => $data['status_id'],
-                    'datetime' => date('Y-m-d H:i:s'),
-                    'action' => 'event_add',
-                    'subject_contact_id' => null,
-                ]
-            );
+        $calendar = stts()->getModel('waContactCalendars')->getById($data['calendar_id']);
+        if (!empty($calendar)) {
+            $data['summary'] = !empty($data['summary']) ? $data['summary'] : $calendar['default_status'];
+
+            if (!$status->getCalendarId()
+                || ($status->getCalendarId() && $status->getDays() > 1)
+                || !empty($data['brand_new'])
+            ) {
+                $data['status_id'] = $waContactEventsModel->insertEvent(
+                    [
+                        'calendar_id' => $data['calendar_id'],
+                        'summary' => $data['summary'],
+                        'start' => $date->format('Y-m-d H:00:00'),
+                        'end' => min(
+                            $date->format('Y-m-d 23:59:59'),
+                            (clone $date)->modify('+1 hour')->format('Y-m-d H:00:00')
+                        ),
+                        'is_allday' => 1,
+                        'is_status' => 1,
+                        'summary_type' => !empty($data['summary']) ? 'custom' : 'default',
+                        'update_datetime' => date('Y-m-d H:i:s'),
+                        'description' => '',
+                        'location' => '',
+                    ]
+                );
+                (new waLogModel())->insert(
+                    [
+                        'app_id' => 'team',
+                        'contact_id' => wa()->getUser()->getId(),
+                        'params' => $data['status_id'],
+                        'datetime' => date('Y-m-d H:i:s'),
+                        'action' => 'event_add',
+                        'subject_contact_id' => null,
+                    ]
+                );
+            } else {
+                $waContactEventsModel->updateById(
+                    $status->getStatusId(),
+                    [
+                        'summary' => $data['summary'],
+                        'calendar_id' => $data['calendar_id'],
+                        'summary_type' => 'custom',
+                    ]
+                );
+                (new waLogModel())->insert(
+                    [
+                        'app_id' => 'team',
+                        'contact_id' => wa()->getUser()->getId(),
+                        'params' => $status->getStatusId(),
+                        'datetime' => date('Y-m-d H:i:s'),
+                        'action' => 'event_edit',
+                        'subject_contact_id' => null,
+                    ]
+                );
+            }
         } else {
-            $waContactEventsModel->updateById(
-                $status->getStatusId(),
-                [
-                    'summary' => $data['summary'],
-                    'calendar_id' => $data['calendar_id'],
-                    'summary_type' => 'custom',
-                ]
-            );
-
+            $waContactEventsModel->deleteById($status->getStatusId());
             (new waLogModel())->insert(
                 [
                     'app_id' => 'team',
                     'contact_id' => wa()->getUser()->getId(),
                     'params' => $status->getStatusId(),
                     'datetime' => date('Y-m-d H:i:s'),
-                    'action' => 'event_edit',
+                    'action' => 'event_delete',
                     'subject_contact_id' => null,
                 ]
             );
         }
 
-//        if (!empty($data['brand_new'])) {
         $view = wa()->getView();
         $view->assign(
             [
@@ -102,8 +107,5 @@ class statusTodaystatusSaveController extends statusJsonController
         $this->response = $view->fetch(
             wa()->getAppPath('templates/actions/todaystatus/TodaystatusList.html', 'status')
         );
-//        } else {
-//            $this->response = $data;
-//        }
     }
 }
