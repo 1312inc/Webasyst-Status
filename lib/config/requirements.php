600b028e07d1c6815b14ba21880b71d0fb8949ff
Kirill Maramygin <kirill.maramygin@gmail.com>
2019-12-02 12:49:41 +0300

    requirements.php + trash files

---
+++ lib/config/requirements.php
@@ -0,0 +1,11 @@
+<?php
+return array(
+    'php' => array(
+        'strict'  => true,
+        'version' => '>=5.6',
+    ),
+    'app.installer' => array(
+        'version' => 'latest',
+        'strict'  => true,
+    ),
+);
