2f4a54dfa08708532ea0ba6d869e20e107a6f09c
Kirill Maramygin <kirill.maramygin@gmail.com>
2021-05-04 23:39:51 +0300

    11.3:Status @ demo: empty user ID

--- lib/config/statusConfig.class.php
+++ lib/config/statusConfig.class.php
@@ -257,6 +257,14 @@ class statusConfig extends waAppConfig
     {
         if ($this->user === null) {
             $this->user = $this->getEntityRepository(statusUser::class)->findByContact(wa()->getUser());
+            if (!$this->user->getId()) {
+                try {
+                    (new statusMissingUserFixer())->fix();
+                    $this->user = $this->getEntityRepository(statusUser::class)->findByContact(wa()->getUser());
+                } catch (Exception $e) {
+                    $this->getLogger()->error('Fail on fix users', $e);
+                }
+            }
         }
 
         return $this->user;
