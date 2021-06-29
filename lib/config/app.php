4cceedf308e9c3dfd6ba76cf46bbe7d0aea1d1d3
vofka <vofka@webasyst.com>
2021-06-29 07:49:32 +0300

    RC version up 2.0.0

--- lib/config/app.php
+++ lib/config/app.php
@@ -2,7 +2,7 @@
 return array (
   'name' => 'Status',
   'icon' => 'img/status.png',
-  'version' => '1.2.0',
+  'version' => '2.0.0',
   'vendor' => '1021997',
   'rights' => true,
   'ui' => '1.3,2.0',
