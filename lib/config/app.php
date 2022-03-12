3781854c95c7743152435583956a2401dba83d56
vofka <vofka@webasyst.com>
2022-03-12 14:04:23 +0300

    new spiral app icon (v4)!

--- lib/config/app.php
+++ lib/config/app.php
@@ -1,8 +1,8 @@
 <?php
 return array (
   'name' => 'Status',
-  'icon' => 'img/status-512.png',
-  'version' => '2.1.0',
+  'icon' => 'img/status.png',
+  'version' => '2.2.0',
   'vendor' => '1021997',
   'rights' => true,
   'ui' => '1.3,2.0',
